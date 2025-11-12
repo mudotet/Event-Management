0989c4b7820a80c838da42521b3e467b8c79151f
mudotet <mudotet@gmail.com>
2025-11-12 22:58:03 +0700

    testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
-import 'package:event_manager/event/event_view.dart';
+// main.dart
+
 import 'package:flutter/material.dart';
-import 'package:event_manager/l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:syncfusion_localizations/syncfusion_localizations.dart';
+import '../event/event_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,20 +11,29 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: [
-        AppLocalizations.delegate,
+      debugShowCheckedModeBanner: false,
+      title: 'Event Manager Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // --- Thêm cấu hình Localization cho Syncfusion Calendar ---
+      localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
+        // Delegate của Syncfusion để hiển thị ngôn ngữ Việt
+        SfGlobalLocalizations.delegate,
       ],
-      supportedLocales: [
+      supportedLocales: const [
         Locale('en', ''), // English
         Locale('vi', ''), // Vietnamese
       ],
-      locale: Locale('vi'),
+      // Đặt ngôn ngữ mặc định là Tiếng Việt
+      locale: const Locale('vi'),
+      // ------------------------------------------------------------
       home: const EventView(),
     );
   }
